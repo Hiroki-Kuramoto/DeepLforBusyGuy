0ed76194edc60cd02e822a452ba35e9210ac545c
Hiroki Kuramoto <kuramoto.hiroki.359@s.kyushu-u.ac.jp>
2022-11-09 23:06:31 +0900

    update

--- main.py
+++ main.py
@@ -17,52 +17,51 @@ import deepl
 DEEPL_KEY = "fa64391c-a63c-170c-0dd7-cf9bc6fec5e6:fx"
 
 def main():
-    """
-    get leftTop coordinates
-    """
-    print("\033[32m\033[1m topLeft\033[0m : \033[32mposition cursor and press [Enter].\033[0m")
-    point = ""
     while True:
-        # interrupt processing
-        if keyboard.is_pressed("enter"):
-            break
-        print("\b"*len(point),end="")
-        print(" "*len(point),end="")
-        print("\b"*len(point),end="",flush=True)
-        point = str(pyautogui.position())
-        print(point, end="", flush=True)
-        time.sleep(0.01)
-    print("Enter was pressed >> ", end="")
-    topLeft = pyautogui.position()
-    print(f"topLeft = {topLeft}")
-    time.sleep(1)
-    """
-    get bottomRight coordinates
-    """
-    print("\033[32m\033[1m bottomRight\033[0m : \033[32mposition cursor and press [Enter].\033[0m")
-    point = ""
-    while True:
-        # interrupt processing
-        if keyboard.is_pressed("enter"):
-            break
-        print("\b"*len(point),end="")
-        print(" "*len(point),end="")
-        print("\b"*len(point),end="",flush=True)
-        point = str(pyautogui.position())
-        print(point, end="", flush=True)
-        time.sleep(0.01)
-    print("Enter was pressed >> ", end="")
-    bottomRight = pyautogui.position()
-    print(f"bottomRight = {bottomRight}")
+        """
+        get leftTop coordinates
+        """
+        print("\033[32m\033[1m topLeft\033[0m : \033[32mposition cursor and press [SPACE].\033[0m")
+        point = ""
+        while True:
+            # interrupt processing
+            if keyboard.is_pressed("space"):
+                break
+            print("\b"*len(point),end="")
+            print(" "*len(point),end="")
+            print("\b"*len(point),end="",flush=True)
+            point = str(pyautogui.position())
+            print(point, end="", flush=True)
+            time.sleep(0.01)
+        print("Enter was pressed >> ", end="")
+        topLeft = pyautogui.position()
+        print(f"topLeft = {topLeft}")
+        time.sleep(1)
+        """
+        get bottomRight coordinates
+        """
+        print("\033[32m\033[1m bottomRight\033[0m : \033[32mposition cursor and press [SPACE].\033[0m")
+        point = ""
+        while True:
+            # interrupt processing
+            if keyboard.is_pressed("space"):
+                break
+            print("\b"*len(point),end="")
+            print(" "*len(point),end="")
+            print("\b"*len(point),end="",flush=True)
+            point = str(pyautogui.position())
+            print(point, end="", flush=True)
+            time.sleep(0.01)
+        print("Enter was pressed >> ", end="")
+        bottomRight = pyautogui.position()
+        print(f"bottomRight = {bottomRight}")
 
-    XYXY = (topLeft.x*2, topLeft.y*2, bottomRight.x*2, bottomRight.y*2)
-    print(f"{XYXY =}")
-    print("main >> All setups completed.")
-    # stdinを1行だけクリアする
-    sys.stdin.read(1)
+        XYXY = (topLeft.x*2, topLeft.y*2, bottomRight.x*2, bottomRight.y*2)
+        print(f"{XYXY =}")
+        print("main >> All setups completed.")
+        # stdinを1行だけクリアする
 
-    while True:
-        input("\033[32m>> Press [Enter].\033[0m")
+
         img:Image = getScreenshot(XYXY)
         doc:str   = image2doc(img)
         assert type(doc) == str, "Type Error: image2doc() returned non-string type."
@@ -72,9 +71,8 @@ def main():
         doc  = "\n\n".join(docs)
         # print(f"\n\033[33m{doc}\033[0m\n")
         result = translator.translate_text(doc, source_lang="EN", target_lang="JA").text
-        print(f"\033[3m{result}\033[0m")
-        time.sleep(1)
-        sys.stdin.flush()
+        print(f"\n* * * * * * * * * * * * * * * * * * * *\n\033[33m{result}\033[0m\n* * * * * * * * * * * * * * * * * * * *\n")
+
 
 
 if __name__ == "__main__":
