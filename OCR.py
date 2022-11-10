d62c0af5175499a8c1040c4f9e02c984da3e2cdb
H.KURAMOTO <68832594+Hiroki-Kuramoto@users.noreply.github.com>
2022-11-10 18:17:55 +0900

    Update OCR.py

--- OCR.py
+++ OCR.py
@@ -4,9 +4,9 @@ from PIL import Image
 
 def main():
     img = Image.open("Image_path")
-    doc = image2doc(img)
+    doc = image2doc(img, "eng")
 
-def image2doc(img:Image):
+def image2doc(img:Image, OCR_LANG):
     """
      using OCR, extract documents from the image
     """
@@ -15,9 +15,9 @@ def image2doc(img:Image):
         raise Exception("image2doc >>\033[31m No OCR tool found\033[0m")
     tool = tools[0]
     langs = tool.get_available_languages()
-    if "eng" not in langs:
+    if OCR_LANG not in langs:
         raise Exception("image2doc >>\033[31m The OCR tool does not support English\033[0m")
-    lang = "eng"
+    lang = OCR_LANG
     
     doc = tool.image_to_string(img,lang=lang,builder=pyocr.builders.TextBuilder())
     
